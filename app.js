e749c1ced05c39dbd58b75f982d87fc530a703b2
vlisak <vlisakml@gmail.com>
2022-10-10 17:05:21 +0200

    Update verze

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const appVersion = "1.0.6";
+const appVersion = "1.0.7";
 
 var appScript = document.querySelector('script[src="app.js"]');
 appScript.src = "app.js?v=" + appVersion;
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = "staticv1-0-6";
+const version = "staticv1-0-7";
 
 self.addEventListener("install", e => {
   e.waitUntil(
